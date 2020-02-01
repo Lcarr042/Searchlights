1e7b65bc0036ca505f97a63c8a8ac6f8fa779010
lcarr042 <lcarr042@ucr.edu>
2020-01-31 22:01:44 -0800

    main game

---
+++ object.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class script : MonoBehavior {
+
+    public GameObject cube;
+
+    public void Enable() {
+        cube.SetActive(true);
+    }
+    public void Disable() {
+        cube.SetActive(false);
+    }
+}
